e11f98f62a71554b364e15477a17f0c58f15bee7
keeeeepin <ducknuttt@gmail.com>
2025-01-07 22:59:48 +0900

    feat : channel 목록 조회에 필요한 query 추가

--- src/main/java/com/ziczic/be/workspace/repository/WorkspaceRepository.java
+++ src/main/java/com/ziczic/be/workspace/repository/WorkspaceRepository.java
@@ -1,6 +1,7 @@
 package com.ziczic.be.workspace.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,6 @@ import com.ziczic.be.workspace.entity.Workspace;
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
 	@Query("SELECT w FROM Workspace w JOIN WorkspaceMember wm ON w.id = wm.workspace.id WHERE wm.member.id = :memberId")
 	List<Workspace> findAllByMemberId(@Param("memberId") Long memberId);
+
+	Optional<Workspace> findById(Long id);
 }
