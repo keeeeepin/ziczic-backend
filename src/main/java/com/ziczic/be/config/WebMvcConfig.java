c1f571309df9b24a914696afb8852d7bbc680dae
keeeeepin <ducknuttt@gmail.com>
2025-05-07 21:18:06 +0900

    fix : WebMvcConfig

--- src/main/java/com/ziczic/be/config/WebMvcConfig.java
+++ src/main/java/com/ziczic/be/config/WebMvcConfig.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	private static final String DEVELOP_FE_ORIGINS = "http://localhost:*";
+	private static final String DOCKER_FE_ORIGINS = "http://ziczic-fe-container:*";
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns(DEVELOP_FE_ORIGINS)
+			.allowedOriginPatterns(DEVELOP_FE_ORIGINS, DOCKER_FE_ORIGINS)
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("*")
 			.allowCredentials(true)
